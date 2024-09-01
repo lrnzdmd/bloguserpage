e69cabfb6dfab5808c9f1a529e539ef2192f082b
lrnzdmd <170991996+lrnzdmd@users.noreply.github.com>
2024-09-02 00:54:19 +0200

    little changes to style on smaller screens, comment submit button overflowed out of the screen

--- src/views/PostPage.vue
+++ src/views/PostPage.vue
@@ -90,19 +90,19 @@ import LoadingSpinner from '@/components/icons/LoadingSpinner.vue';
 
     <div class="bg-black w-5/6  self-center mx-auto" style="height: 1px;"></div>
 
-    <div class="flex justify-center mt-8">
+    <div class="flex justify-center mt-8 sm:w-full">
         <p v-if="errorMsg" class="text-lg text-red-500">{{ errorMsg }}</p>
-        <div v-if="posts.length > 0" class="w-3/4">
-            <h3 class="self-start px-8 text-2xl font-bold text-gray-700">Comments</h3>
-            <div class="px-8 py-8" v-for="comment in posts[index].comments" :key="comment.id">
+        <div v-if="posts.length > 0" class="w-3/4 ">
+            <h3 class="self-start lg:px-8 sm:px-2 text-2xl font-bold text-gray-700">Comments</h3>
+            <div class="lg:px-8 sm:px-2 py-8" v-for="comment in posts[index].comments" :key="comment.id">
                 <CommentCard :username="comment.commenter.username" :text="comment.text"
                     :created-at="formatDate(comment.createdAt)"></CommentCard>
             </div>
             <div class="bg-black w-full  self-center mx-auto" style="height: 1px;"></div>
             
-                <form @submit.prevent v-if="isLoggedIn" class="flex flex-col w-full px-8 mt-4" action="" method="post">
-        <textarea v-model="commentText" class="w-full py-3 mt-2 text-gray-700 bg-white border rounded-lg px-4  focus:border-sky-400  focus:ring-sky-300 focus:outline-none focus:ring focus:ring-opacity-40" required name="message" id="message" placeholder='New comment...'></textarea>
-        <button @click="handleSubmit" class="w-1/5 self-end mt-2 px-6 py-3 text-md font-semibold text-white tracking-wide capitalize transition-colors duration-300 transform bg-sky-600  shadow-gray-950 rounded-lg hover:bg-sky-500 focus:outline-none focus:ring focus:ring-sky-300 focus:ring-opacity-50" type="submit">Submit</button>
+                <form @submit.prevent v-if="isLoggedIn" class="flex flex-col w-full lg:px-8 mt-4" action="" method="post">
+        <textarea v-model="commentText" class="w-full sm:w-full py-3 mt-2 text-gray-700 bg-white border rounded-lg px-4  focus:border-sky-400  focus:ring-sky-300 focus:outline-none focus:ring focus:ring-opacity-40" required name="message" id="message" placeholder='New comment...'></textarea>
+        <button @click="handleSubmit" class="w-1/5 self-end mt-2 px-2 py-3 text-md font-semibold text-white tracking-wide capitalize transition-colors duration-300 transform bg-sky-600  shadow-gray-950 rounded-lg hover:bg-sky-500 focus:outline-none focus:ring focus:ring-sky-300 focus:ring-opacity-50" type="submit">Submit</button>
     </form>
 
 
